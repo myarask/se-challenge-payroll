2db0e94998e2274dafe79275f6c60464667aee90
Mark Yaraskavitch <mark@yaraskavitch.com>
2024-02-10 12:30:23 -0500

    load time report data

--- backend/src/time-reports/dto/create-time-report.dto.ts
+++ backend/src/time-reports/dto/create-time-report.dto.ts
@@ -1,1 +1,8 @@
 export class CreateTimeReportDto {}
+
+export class UploadTimeReportDto {
+  'date': string;
+  'hours worked': number;
+  'employee id': string;
+  'job group': 'A' | 'B';
+}
--- backend/src/time-reports/time-reports.controller.ts
+++ backend/src/time-reports/time-reports.controller.ts
@@ -1,6 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UploadedFile,
+  UseInterceptors,
+  ParseFilePipeBuilder,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
 import { TimeReportsService } from './time-reports.service';
-import { CreateTimeReportDto } from './dto/create-time-report.dto';
+import {
+  CreateTimeReportDto,
+  UploadTimeReportDto,
+} from './dto/create-time-report.dto';
+import Papa from 'papaparse';
 import { UpdateTimeReportDto } from './dto/update-time-report.dto';
 
 @Controller('time-reports')
@@ -23,12 +40,59 @@ export class TimeReportsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTimeReportDto: UpdateTimeReportDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateTimeReportDto: UpdateTimeReportDto,
+  ) {
     return this.timeReportsService.update(+id, updateTimeReportDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.timeReportsService.remove(+id);
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('upload')
+  async uploadFile(
+    @Body() body: UploadTimeReportDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'csv',
+        })
+        .build({
+          fileIsRequired: true,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    // Get the time report id from the file name
+    const filenameParts = file.originalname
+      .toLowerCase()
+      .replace('.csv', '')
+      .split('-');
+    const timeReportId = +filenameParts[filenameParts.length - 1];
+
+    // Parse the CSV file
+    const content = await Papa.parse(file.buffer.toString(), {
+      header: true,
+      dynamicTyping: true,
+      skipEmptyLines: true,
+    });
+
+    // Format the data
+    const data = content.data.map((row: Record<string, unknown>) => {
+      return {
+        date: row['date'],
+        hoursWorked: row['hours worked'],
+        employeeId: row['employee id'],
+        jobGroup: row['job group'],
+      };
+    });
+
+    console.log(data);
+
+    return this.timeReportsService.create(timeReportId);
   }
 }
